472d1593c2af02cdc9a06ec0fa0926a91818ebc9
archiecheng <archiechengice@outlook.com>
2024-04-18 14:11:42 -0700

    1.Blog project established 2.Create blog templates

---
+++ myauth/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/4/18 13:30
+# @Author  : Laiyong(Archie) Cheng
+# @Site    : 
+# @File    : urls.py
+# @Software: PyCharm
+from django.urls import path
+from . import views
+app_name = 'myauth'
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+]
